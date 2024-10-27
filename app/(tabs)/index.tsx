c45bf8d9adf7cb9baaf12c546cef661630230132
shuvo-me <shuvo.js.dev@gmail.com>
2024-10-27 17:31:08 +0600

    fix: enhance button component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { View, StyleSheet } from "react-native";
 import { Image } from "expo-image";
 import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
 
 const PlacholderImage = require("@/assets/images/background-image.png");
 
@@ -9,6 +10,10 @@ export default function Index() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlacholderImage} />
+      </View>
+      <View>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -19,14 +24,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#25292e",
     alignItems: "center",
-    justifyContent: "center",
   },
   imageContainer: {
     flex: 1,
   },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
